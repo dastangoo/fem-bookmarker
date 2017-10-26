0bdbbae24a89c5f72fb2345467363f844c87d48b
dastangoo <hamed.dastangoo@gmail.com>
2017-10-26 21:44:52 +0330

    Bookmark Application

--- app/renderer.js
+++ app/renderer.js
@@ -1,47 +1,50 @@
-const { shell, remote } = require('electron');
-const systemPreferences = remote.systemPreferences;
+// const os = require('os');
+// const fs = require('fs');
+
+// const files = fs.readdirSync(os.homedir());
+// console.log(files)
+
+// files.forEach(name => {
+//   const file = document.createElement('li');
+//   file.textContent = name;
+//   document.body.appendChild(file);
+// });
 
 const newLinkUrl = document.querySelector('#new-link-url');
 const newLinkSubmit = document.querySelector('.new-link-form--submit');
 const newLinkForm = document.querySelector('.new-link-form');
 const linkTemplate = document.querySelector('#link-template');
-const linksSection = document.querySelector('.links');
-
-linksSection.addEventListener('click', (event) => {
-  if (event.target.href) {
-    event.preventDefault();
-    shell.openExternal(event.target.href);
-  }
-});
 
 newLinkUrl.addEventListener('keyup', () => {
   newLinkSubmit.disabled = !newLinkUrl.validity.valid;
 });
 
+
 const parser = new DOMParser();
 const parseResponse = (text) => parser.parseFromString(text, 'text/html');
 const findTitle = (nodes) => nodes.querySelector('title').textContent;
-
 const addToPage = ({ title, url }) => {
   const newLink = linkTemplate.content.cloneNode(true);
   const titleElement = newLink.querySelector('.link--title');
   const urlElement = newLink.querySelector('.link--url');
-
+  const linkSelection = document.querySelector('.links');
   titleElement.textContent = title;
   urlElement.href = url;
   urlElement.textContent = url;
 
-  linksSection.appendChild(newLink);
+  linkSelection.appendChild(newLink);
   return { title, url };
 };
-
-newLinkForm.addEventListener('submit', () => {
+newLinkForm.addEventListener('submit', (event) => {
   event.preventDefault();
 
   const url = newLinkUrl.value;
 
+  // console.log(url);
+
   fetch(url)
     .then(response => response.text())
+    // .then(response => console.log(response))
     .then(parseResponse)
     .then(findTitle)
     .then(title => ({ title, url }))
@@ -50,8 +53,60 @@ newLinkForm.addEventListener('submit', () => {
     .catch(error => console.error(error));
 });
 
-window.addEventListener('load', () => {
-  if (systemPreferences.isDarkMode()) {
-    document.querySelector('link').href = 'styles-dark.css';
-  }
-});
+// const { shell, remote } = require('electron');
+// const systemPreferences = remote.systemPreferences;
+
+// const newLinkUrl = document.querySelector('#new-link-url');
+// const newLinkSubmit = document.querySelector('.new-link-form--submit');
+// const newLinkForm = document.querySelector('.new-link-form');
+// const linkTemplate = document.querySelector('#link-template');
+// const linksSection = document.querySelector('.links');
+
+// linksSection.addEventListener('click', (event) => {
+//   if (event.target.href) {
+//     event.preventDefault();
+//     shell.openExternal(event.target.href);
+//   }
+// });
+
+// newLinkUrl.addEventListener('keyup', () => {
+//   newLinkSubmit.disabled = !newLinkUrl.validity.valid;
+// });
+
+// const parser = new DOMParser();
+// const parseResponse = (text) => parser.parseFromString(text, 'text/html');
+// const findTitle = (nodes) => nodes.querySelector('title').textContent;
+
+// const addToPage = ({ title, url }) => {
+//   const newLink = linkTemplate.content.cloneNode(true);
+//   const titleElement = newLink.querySelector('.link--title');
+//   const urlElement = newLink.querySelector('.link--url');
+
+//   titleElement.textContent = title;
+//   urlElement.href = url;
+//   urlElement.textContent = url;
+
+//   linksSection.appendChild(newLink);
+//   return { title, url };
+// };
+
+// newLinkForm.addEventListener('submit', () => {
+//   event.preventDefault();
+
+//   const url = newLinkUrl.value;
+
+//   fetch(url)
+//     .then(response => response.text())
+//     .then(parseResponse)
+//     .then(findTitle)
+//     .then(title => ({ title, url }))
+//     .then(addToPage)
+//     .then(title => console.log(title))
+//     .catch(error => console.error(error));
+// });
+
+// window.addEventListener('load', () => {
+//   if (systemPreferences.isDarkMode()) {
+//     document.querySelector('link').href = 'styles-dark.css';
+//   }
+// });
